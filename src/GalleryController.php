0d7563ced78836161b08afee30ac6681c2bd4e57
Henrikas <henrikasju@gmail.com>
2019-11-19 13:10:50 +0200

    updated gitignore started working on gallerycontroller

---
+++ src/GalleryController.php
@@ -0,0 +1,19 @@
+<?php
+
+
+class GalleryController extends MainController implements iController
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+    public function printPageView()
+    {
+        // TODO: Implement printPageView() method.
+    }
+
+    public function getTitle()
+    {
+        // TODO: Implement getTitle() method.
+    }
+}
